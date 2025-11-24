d3943f36b35d6a0ac5c1f0229ce3910e99bad83d
Aringas Civilka <aringas15@gmail.com>
2025-11-24 20:33:32 +0200

    Fixed dark/light mode and added a nice button.

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,11 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleBtn = document.getElementById('theme-toggle');
-    const currentTheme = localStorage.getItem('theme') || 'dark';
+    const darkIcon = document.getElementById("dark-icon");
+    const lightIcon = document.getElementById("light-icon");
+    const currentTheme = localStorage.getItem('theme') || 'light';
     document.documentElement.setAttribute('data-theme', currentTheme);
 
+    setIcon(currentTheme)
+
     toggleBtn.addEventListener('click', function () {
+        toggleTheme(lightIcon, darkIcon);
+    });
+
+    function toggleTheme() {
         const newTheme = document.documentElement.getAttribute('data-theme') === 'light' ? 'dark' : 'light';
+        if (newTheme === 'light') {
+            setIcon('light')
+        }
+        else {
+            setIcon('dark')
+        }
         document.documentElement.setAttribute('data-theme', newTheme);
         localStorage.setItem('theme', newTheme);
-    });
+    }
+    function setIcon(theme) {
+        lightIcon.style.display = theme === 'light' ? 'none' : 'inline';
+        darkIcon.style.display = theme === 'dark' ? 'none' : 'inline';
+    }
 });
+
+
