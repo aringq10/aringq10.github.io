2ae076145e9f07d7ac4bc7dd340a33450d8acebb
Aringas Civilka <aringas15@gmail.com>
2025-11-27 01:50:51 +0200

    Added a favicon in /assets, cleaned up the theme-toggle js script and made a "color_theme" variable in _config.yml.

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -2,26 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const toggleBtn = document.getElementById('theme-toggle');
     const darkIcon = document.getElementById("dark-icon");
     const lightIcon = document.getElementById("light-icon");
-    const currentTheme = localStorage.getItem('theme') || 'light';
-    document.documentElement.setAttribute('data-theme', currentTheme);
+    const startingTheme = document.documentElement.getAttribute('data-theme');
 
-    setIcon(currentTheme)
+    if (startingTheme === 'light')
+        setIcon('light');
+    else
+        setIcon('dark');
 
     toggleBtn.addEventListener('click', function () {
-        toggleTheme(lightIcon, darkIcon);
-    });
-
-    function toggleTheme() {
         const newTheme = document.documentElement.getAttribute('data-theme') === 'light' ? 'dark' : 'light';
-        if (newTheme === 'light') {
-            setIcon('light')
-        }
-        else {
-            setIcon('dark')
-        }
+        if (newTheme === 'light')
+            setIcon('light');
+        else
+            setIcon('dark');
         document.documentElement.setAttribute('data-theme', newTheme);
         localStorage.setItem('theme', newTheme);
-    }
+    });
     function setIcon(theme) {
         lightIcon.style.display = theme === 'light' ? 'none' : 'inline';
         darkIcon.style.display = theme === 'dark' ? 'none' : 'inline';
